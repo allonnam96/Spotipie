b7cfd428ed302165c34a86138b637974403b2912
Allon Nam <allon.nam96@gmail.com>
2023-11-10 17:00:48 -0500

    user-auth

---
+++ Backend/frontend/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import SignupFormPage from "./components/SignupFormPage";
+import LoginFormPage from "./components/LoginFormPage";
+import Navigation from "./components/Navigation";
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+      <Navigation />
+        <Switch>
+          <Route path="/login" >
+            <LoginFormPage />
+          </Route>
+          <Route path="/signup">
+            <SignupFormPage />
+          </Route>
+        </Switch>
+    </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
