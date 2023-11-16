08c7d23374341550568f5de8dadcb97b63044f01
Allon Nam <allon.nam96@gmail.com>
2023-11-15 22:26:07 -0500

    navbar css

--- Backend/frontend/src/components/Navigation/index.js
+++ Backend/frontend/src/components/Navigation/index.js
@@ -4,30 +4,47 @@ import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
-function Navigation() {
+const Navigation = () => {
   const sessionUser = useSelector(state => state.session.user);
 
-  let sessionLinks;
-  if (sessionUser) {
-    sessionLinks = (
-      <ProfileButton user={sessionUser} />
-    );
-  } else {
-    sessionLinks = (
-      <>
-        <NavLink to="/login">Log in</NavLink>
-        <NavLink to="/signup">Sign up</NavLink>
-      </>
-    );
-  }
+  // let sessionLinks;
+  // if (sessionUser) {
+  //   sessionLinks = (
+  //     <ProfileButton user={sessionUser} />
+  //   );
+  // } else {
+  //   sessionLinks = (
+  //     <>
+  //       <NavLink to="/login">Log in</NavLink>
+  //       <NavLink to="/signup">Sign up</NavLink>
+  //     </>
+  //   );
+  // }
 
   return (
-    <ul>
-      <li>
-        <NavLink exact to="/">Spotipie</NavLink>
-        {sessionLinks}
-      </li>
-    </ul>
+    <div className="navigationUser">
+      {sessionUser ? (ProfileButton) : (
+      <>
+        {/* <NavLink exact to="/">Spotipie</NavLink> */}
+        <ul>
+          <li>
+            <NavLink to="/signup">
+              <button id="signup" className= "navButton">
+                Sign Up
+              </button>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/login">
+              <button id="login" className= "navButton">
+                Login
+              </button>
+            </NavLink>
+          </li>
+        </ul>
+      </>
+      )}
+    </div>
   );
 }
 
