e60aa86a9f7440d14f37f44507ae339183249c98
Allon Nam <allon.nam96@gmail.com>
2024-01-04 17:04:21 -0500

    rendering

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_27_171134) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_24_201822) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -131,5 +131,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_27_171134) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "albums", "artists"
-  add_foreign_key "songs", "albums", on_delete: :cascade
+  add_foreign_key "songs", "albums"
 end
